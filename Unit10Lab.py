4f7850e323a7be137e1e89a16aa834d9b7f3be18
student <CHOMME@madisoncollege.edu>
2022-04-05 15:58:25 -0500

    Unit 10 lab completed

--- Unit10Lab.py
+++ Unit10Lab.py
@@ -9,52 +9,9 @@ validated, once determined as valid the old address will be removed and the new 
 
 '''
 
-import requests # import statements importing request and json libraries
+import requests # import statements importing request, time, and json libraries
 import json
-
-'''
-
-devices = [
-    {
-        'hostname': 'R1',
-        'type':'router',
-        'brand':'cisco',
-        'mgmtIP':'10.0.0.1'
-        },
-    {
-        'hostname': 'S1',
-        'type':'switch',
-        'brand':'cisco',
-        'mgmtIP':'10.0.0.2'
-        },
-    ] #  devices is a list of dictionaries containing device information
-
-
-def createListSubset(listOfDevs):
-
-    newList = []
-
-    for dev in listOfDevs:
-        newList.append({'hostname':dev['hostname'], 'mgmtIP':dev['mgmtIP']})
-
-    return newList
-
-
-
-
-print(devices)
-
-modList = createListSubset(devices)
-
-print(modList)
-
-for device in modList:
-    print(device)
-
-
-'''
-
-
+import time
 
 
 
@@ -75,15 +32,14 @@ def getIntfRest(mgmtIP): # function responsible for retriveing the nested dictio
     }
 
     response = requests.request("GET", url, auth = (username,password), verify = False, headers=headers, data=json.dumps(payload))
-    # repsone sends the api post call to the location specified in the url variable
+    # repsone sends the api get call to the location specified in the url variable
 
-#    print(response.json())
     
     return response.json() # the response is returned in json to main for further use
 
 
 
-def getIntfRestMAC(mgmtIP):
+def getIntfRestMAC(mgmtIP): # function responsible for retriveing the nested dictionary containing info on the interface statuses 
     
     # lines 19-31 are imported from the turnipTheBeet GIT repository with some alterations to the url variable to pass mgmtIP
 
@@ -100,46 +56,50 @@ def getIntfRestMAC(mgmtIP):
     }
 
     response = requests.request("GET", url, auth = (username,password), verify = False, headers=headers, data=json.dumps(payload))
-    # repsone sends the api post call to the location specified in the url variable
+    # repsone sends the api get call to the location specified in the url variable
 
-#    print(response.json())
-    
+
     return response.json() # the response is returned in json to main for further use
 
 
 
-def combineIntLists(rawintfList,rawintfStateList):
+def combineIntLists(rawintfList,rawintfStateList): # function combining both lists containing the interface status and the interface information
     
 
-    intfList = rawintfList['ietf-interfaces:interfaces']['interface']
+    intfList = rawintfList['ietf-interfaces:interfaces']['interface'] # intfList parses the raw response from getIntfRest to the list of dicitonaries
 
     intfStateList = rawintfStateList['ietf-interfaces:interfaces-state']['interface']
+    # intfStateList parses the raw response from getIntfRestMAC to the list of dicitonaries
     
-    tempList = []
+    tempList = [] # tempList is an empty list designed to keep intfStateList contents in
 
-    combinedList = []
+    combinedList = [] # combinedList is an empty list designed to keep both list contents in
     
-    for interface in intfList:
+    for interface in intfList: # for each of the dictionaries in intfList, if the type value is listed as iana-if-type:ethernetCsmacd,
+                               # the interface names and ip addresses are appended as a dictionary to combinedList
 
         if interface['type'] == 'iana-if-type:ethernetCsmacd':
 
             combinedList.append({'Name':interface['name'], 'IP':interface['ietf-ip:ipv4']['address'][0]['ip']})
 
-    for interface in intfStateList:
-
+    for interface in intfStateList: # for each of the dictionaries in intfStateList, if the type value is listed as iana-if-type:ethernetCsmacd,
+                                    # the interface mac addresses are appended as a dictionary to tempList
+                                    
         if interface['type'] == 'iana-if-type:ethernetCsmacd':
 
             tempList.append({'MAC':interface['phys-address']})
 
-    count = 0
+    count = 0 # count is used to iterate through the items in tempList
         
-    for dictionary in combinedList:
+    for dictionary in combinedList: # for each of the nested dictionaries contained in combinedList, the dictionary needs to be appended with
+                                    # the corrosponding item in tempList to complete the list of dictionaries. count is incremented by one to
+                                    # iterate through the tempList
 
         dictionary.update(tempList[count])
 
-        count += 1    
+        count += 1
     
-    return combinedList
+    return combinedList # the list is returned to main for further use
 
     
 
@@ -148,13 +108,21 @@ def printList(masterList):
 
     # print statements setting up the formatting for the list of interfaces
 
-    print('\n' + 'Interface Name\t\t' + 'IP Address\t\t' + 'MAC\t\t')
+    print('\n' + 'Interface Name\t\t' + 'IP Address\t\t' + 'MAC')
     print('\n' + "-" * 100)
 
 
-    for item in masterList: # for each of the dictionaries in the masterList, print the values of Name, IP, and MAC to match the headers.
+    for item in masterList:
+# for each of the dictionaries in the masterList, print the values of Name, IP, and MAC to match the headers. If the IP value is more than 
+# 13 characters it will need one less tab to properly allign the formatting with the headers.
 
-        print('\n',item['Name'],'\t',item['IP'],'\t\t',item['MAC'],'\t\t')
+        if len(item['IP']) <= 13:
+            
+            print('\n',item['Name'],'\t',item['IP'],'\t\t',item['MAC'])
+
+        else:
+           print('\n',item['Name'],'\t',item['IP'],'\t',item['MAC']) 
+
 
 
 def userInput(prompt, answerList): #User input function
@@ -166,6 +134,7 @@ def userInput(prompt, answerList): #User input function
         answer = input(prompt)
     
     return answer #The answer will be returned once a valid answer is entered.
+
 
 
 def validIP(IP): #Function validating user input ip
@@ -195,68 +164,153 @@ def validIP(IP): #Function validating user input ip
     return validIP # validIP returned to main for further use        
 
 
-def validCIDR(CIDR):
 
-    validCIDR = True
-
-    if CIDR.isalpha() == True:
-
-        validCIDR = False
-        return validCIDR
+def validSubnet(SN): #Function validating user input subnet
     
-    if CIDR.isnumeric() == False:
+    validSN = True #Flag measuring if the subnet is valid or not
+
+    if SN.count('.') != 3: #if loop measuing the ammount of decimals in the subnet for proper formatting
+        validSN = False
+
+    snCheck = SN.split('.') #variable that splits the subnet on the decimals for easier handling of octets
+    
+    subnetList = [] #empty list which will contain octets of subnet
+    
+    if len(snCheck) !=4: #if loop stating that if the ammount of octets is not equal to four, the valid flag will be set false
+        validSN = False
+    
+    for octet in snCheck: #for loop iterating through all items in snCheck
+        subnetList.append(octet)
+        if octet.isnumeric() == False: #if statement checking if the octet string is strictly numeric
+            validSN = False
+            return validSN
+        if int(octet) < 0 or int(octet) > 255: # if statement verifying that the octets are in the valid range for subnet addresses
+            validSN = False
+
+    if int(subnetList[0]) != 255: # if statement ensuring that the first octet is equal to 255
+
+        validSN = False
         
-        validCIDR = False
+    if int(subnetList[1]) < 255 and int(subnetList[2]) != 0: # if statement stating that if the second octet is less than 255, the subsequent 
+                                                             # octet must be 0 to be valid
+        validSN = False
 
-    if int(CIDR) < 1 or int(CIDR) >= 32:
-
-        validCIDR = False
-
-    return validCIDR     
+    if int(subnetList[2]) < 255 and int(subnetList[3]) != 0: # if statement stating that if the third octet is less than 255, the subsequent 
+                                                             # octet must be 0 to be valid
+        validSN = False
+        
+    return validSN # validIP returned to main for further use       
     
+
+
+def changeAddress(mgmtIP,userIP,userMask,userInterface): # function used to push the ip address change to the users parameters on the device
+# lines 197-222 are imported from the turnipTheBeet GIT repository with some alterations to the url and payload variables to pass mgmtIP,
+# userIP,userMask, and userInterface to the API call.
+
+    time.sleep(60) # time is used to delay the program by 60 seconds in order to allow for manual removal of the interface address
+    
+    url = "https://" + mgmtIP + ":443/restconf/data/ietf-interfaces:interfaces/interface=" + userInterface
+# url contains the destination for the api call, with mgmtIPbeing the ip address of the device being passed to the function and userInterface
+# specifying the interface as the parameter in the url     
+
+    
+    username = 'cisco' # username and password contain the username and password to authenticate with the device
+    password = 'cisco'
+    payload={"ietf-interfaces:interface": { # payload iterates through the YANG model using the userInterface variable to specify the interface
+                    "name": userInterface, # userIP as the new address and userMask as the new subnet mask
+                    "description": "Configured by RESTCONF",
+                    "type": "iana-if-type:ethernetCsmacd",
+                    "enabled": "true",
+                                     "ietf-ip:ipv4": {
+                                                            "address": [{
+                                                                "ip": userIP,
+                                                                "netmask": userMask
+                                                                }]}}}
+
+
+    headers = { # headers specify the type of data being requested, and the authorization type
+    'Authorization': 'Basic cm9vdDpEX1ZheSFfMTAm',
+    'Accept': 'application/yang-data+json',
+    'Content-Type': 'application/yang-data+json'
+    }
+
+    response = requests.request("PUT", url, auth=(username,password),headers=headers, verify = False, data=json.dumps(payload))
+    # repsone sends the api put call to the location specified in the url variable
+
+
+
+
 
 ### Main code
 
-ipAddr = '10.10.20.175'
+ipAddr = '10.10.20.175' # ipAddr contains the address of dist-rtr01 
 
-intList =  getIntfRest(ipAddr)
+intList =  getIntfRest(ipAddr) # intList calls the getIntfRest function passing ipAddr to the function
 
-intStateList = getIntfRestMAC(ipAddr)
+intStateList = getIntfRestMAC(ipAddr) # intStateList calls the getIntfRestMAC function passing ipAddr to the function
 
-combinedIntList = combineIntLists(intList, intStateList)
+combinedIntList = combineIntLists(intList, intStateList) # combinedIntList calls the combineIntLists function passing intList and intStateList to the function
 
-printList(combinedIntList)
+printList(combinedIntList) # printList is called, passing combinedIntList to the function
 
-askUser = userInput('Would you like to change an IP address on an interface? [Y or N]: ', ['y','n','Y','N'])
+askUser = userInput('\nWould you like to change an IP address on an interface? [Y or N]: ', ['y','n','Y','N']) # askUser calls the userInput
+                                                                # function to ask if they would like to change an ip address on an interface
 
-if askUser.lower() == 'n':
-    print('No interface addresses were changed at this time.')
+if askUser.lower() == 'n': # if the user declines, a print statement saying nothing has been changed will display
+    print('\nNo interface addresses were changed at this time.')
 
-if askUser.lower() == 'y':
+if askUser.lower() == 'y': # if the user wants to change an interface address, they will be asked for the name of the interface and the
+                           # ip address of the interface. The ip will be checked for validity using validIP
 
-    userIP = input('Please enter the new IP address in x.x.x.x format: ')
+    userInterface = input('\nPlease enter the name of the interface you would like to change exactly as displayed on the above report: ')
+    userIP = input('\nPlease enter the new IP address in x.x.x.x format: ')
     validateIP = validIP(userIP)
 
         
     while validateIP == False: # if the ip address is not valid the user will be asked to enter a valid ip address
 
-        print('The entered IP address is not a valid IP address.')
-        userIP = input('Please enter the new IP address in x.x.x.x format: ')
+        print('\nThe entered IP address is not a valid IP address.')
+        userIP = input('\nPlease enter the new IP address in x.x.x.x format: ')
         validateIP = validIP(userIP)
         
     else:
-        userCIDR = input('Please enter the length of the subnet mask for this address (ex 24 = /24 = 255.255.255.0): ')
+        userSubnet = input('\nWhat is the subnet of this IP address? (x.x.x.x format, class A or lower subnets only): ')
+        # userSubnet asks the user for the subnet of the above ip address
+        
+        subnetCheck = validSubnet(userSubnet) # subnetCheck validates the subnet
 
-        validateCIDR = validCIDR(userCIDR)
 
-        while validateCIDR == False:
+        while subnetCheck == False: # if the subnet is deemed not valid, an error statement will print and the user will be prompted 
+                                    # again for a subnet address that will be validated again 
+
+            print('\nThe entered subnet is invalid.')
+
+            userSubnet = input('\nWhat is the subnet of this IP address? (x.x.x.x format, class B or lower subnets only): ')
+
+            subnetCheck = validSubnet(userSubnet)
+
+
+        else: 
+            print('\nManually clear out the IP address of this interface, you have 60 seconds to do so before the code automatically executes.')
             
-            print('The entered subnet mask length is not valid.')
-            userCIDR = input('Please enter the length of the subnet mask for this address (ex 24 = /24 = 255.255.255.0): ')
-            validateCIDR = validCIDR(userCIDR)
+            updateDevice = changeAddress(ipAddr,userIP,userSubnet,userInterface)
 
-        else:
-            print('valid')
+            intList =  getIntfRest(ipAddr)
+
+            intStateList = getIntfRestMAC(ipAddr)
+
+            combinedIntList = combineIntLists(intList, intStateList)
+
+            printList(combinedIntList)
+
+
+'''
+Once both the subnet and the ip are validated, a message states that the user must manually clear the address from the device to prevent
+secondary addresses from accumulating on the interface. Once 60 seconds has passed, the changeAddress function passes pAddr,userIP,userSubnet,
+and userInterface to make the specified changes to the device, and the intList, intStateList, combinedIntList and printList functions are
+re-ran to collect the correct information about the devices and reprint the report showing the newly modified address on the interface
+'''
+
 
 
 
